0463be72396e93cb46929a675e6dca16c0dad03f
luan.votrong <vtluan288@gmail.com>
2016-12-31 04:59:13 +0700

    - Update demo

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		rigidbody.AddForce (new Vector3(10.0f, 0.0f, 0.0f), ForceMode.Impulse);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		Debug.Log ("trigger 3d");
+	}
+
+	void OnCollisionEnter2D(Collision2D col2d) {
+		Debug.Log ("trigger 2d");
+	}
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+	public GameObject mainCamera;
+	public GameObject prefabBall;
+	public GameObject prefabTerrain;
+
+	private GameObject ball;
+	private GameObject terrain;
+
+	// Use this for initialization
+	void Start () {
+		terrain = Instantiate (prefabTerrain);
+		ball = Instantiate (prefabBall);
+
+		Transform transformCamera = mainCamera.GetComponent<Transform> ();
+		Vector3 ballPos = ball.transform.position;
+		transformCamera.position = new Vector3(ballPos.x, ballPos.y+20, ballPos.z);
+		transformCamera.rotation = Quaternion.Euler (90.0f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("fdsafsfd");
+	}
+}
